6fe82705f6c950aa4db299265b5810c706f4bb65
the7 <one_byte@qq.com>
2020-03-03 10:13:52 +0800

    first commit

---
+++ Data/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chess.Data
+{
+    public struct Point
+    {
+        public int Row { get; set; }
+        public int Cell { get; set; }
+    }
+
+    public enum Level
+    {
+  
+        活3 = 1,
+        眠3 = 2
+
+    }
+
+    public class ValuedPoint
+    {
+        public Point Point { get; set; }
+        public int Score { get; set; }
+
+        public int Level { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+基于Blazor的简单五子棋程序
+
+演示地址 ： http://chess.ut32.com
+
+![](https://static.ut32.com/upload/cb6c8fc936b14194ad40d600f7b02891.png) 
